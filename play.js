5f662d368bdaa63ac271fc7f642ab2c8926f6104
ChaiUrs <mysorechaitra@gmail.com>
2019-08-01 00:17:11 +0000

    Implemented Snake-Client - Refactored &  added constants

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 
 const { connect } = require('./client');
-const {setupInput} = require('./input');
+const { setupInput } = require('./input');
 
 console.log('Connecting to the game server...');
 
